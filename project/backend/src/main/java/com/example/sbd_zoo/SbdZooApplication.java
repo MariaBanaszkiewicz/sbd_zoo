9f023007463100f7f516f0c05bde812f7782245b
MariaBanaszkiewicz <xmox@interia.pl>
2022-12-17 17:56:55 +0100

    initialize backend

---
+++ project/backend/src/main/java/com/example/sbd_zoo/SbdZooApplication.java
@@ -0,0 +1,14 @@
+package com.example.sbd_zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdZooApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SbdZooApplication.class, args);
+		System.out.println("hello world!");
+	}
+
+}
---
+++ project/backend/src/test/java/com/example/sbd_zoo/SbdZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbd_zoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
